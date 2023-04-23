a1499ef8fc6d32897cecd65392f605027bb60d85
EC2 Default User <ec2-user@ip-10-20-33-222.eu-central-1.compute.internal>
2023-04-23 17:57:20 +0000

    edited 9

--- cleanup.sh
+++ cleanup.sh
@@ -4,8 +4,7 @@ read -e -p 'Enter cluster name: ' -i "SE-EDI-DEV-EKS-CLUSTER" eks_cluster
 
 echo "....................................uninstall aws-load-balancer-controller.................."
 helm uninstall aws-load-balancer-controller eks/aws-load-balancer-controller \
-  -n kube-system \
-  --set clusterName=$eks_cluster
+  -n kube-system 
 echo ".........................deleting service account............................."
 eksctl delete iamserviceaccount \
   --cluster=$eks_cluster \
